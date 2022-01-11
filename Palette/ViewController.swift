38f28c790758ee6864188d59de62aa19491d8818
Justin Lowry <github@lowry.life>
2022-01-10 22:35:25 -0700

    First commit

---
+++ Palette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Palette
+//
+//  Created by Justin Lowry on 1/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
